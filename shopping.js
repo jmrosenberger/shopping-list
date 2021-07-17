e82342fc16f40fd4281bf1d00ec6f1c95cdbbcc9
jmrosenberger <jmrosenberger0811@yahoo.com>
2021-07-17 00:48:10 -0500

    final edits

--- shopping.js
+++ shopping.js
@@ -2,48 +2,68 @@ const shoppingList = [
     {
         id: 1,
         name: "milk",
-        price: 3.00
+        price: 3.00,
+        quantity: 1,
+        store: "publix",
+        section: "dairy",
+        frequency: 7,
+        brand: "publix"
     },
     {
         id: 2,
         name: "eggs",
-        price: 2.50
+        price: 2.50,
+        quantity: 2,
+        store: "publix",
+        section: "dairy",
+        frequency: 7,
+        brand: "publix"
     },
     {
         id: 3,
         name: "pizza rolls",
-        price: 4.45
+        price: 4.45,
+        quantity: 2,
+        store: "publix",
+        section: "frozen foods",
+        frequency: 7,
+        brand: "publix"
     },
     {
         id: 4,
         name: "protein",
-        price: 65.00
+        price: 65.00,
+        quantity: 1,
+        store: "publix",
+        section: "misc",
+        frequency: 7,
+        brand: "publix"
     }
 ]
 
-// console.log(shoppingList);
 
 const toiletPaper = {
         name: "toilet paper",
-        price: 10.00
+        price: 10.00,
+        quantity: 12,
+        store: "publix",
+        section: "paper goods",
+        frequency: 7,
+        brand: "publix"
 };
 
 
 const addToShoppingList = (listItem) => {
     const lastIndex = shoppingList.length - 1
     const currentLastListItem = shoppingList[lastIndex]
-    // const maxId = currentLastListItem.id
     const idForNewListItem = currentLastListItem.id + 1
 
     listItem.id = idForNewListItem
     shoppingList.push(listItem)
+    
 }
 
 addToShoppingList(toiletPaper)
 
 console.log(shoppingList)
 
-for (const listItem of shoppingList) {
-    console.log(shoppingList)
-    
-}
